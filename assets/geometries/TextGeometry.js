d6eb804ae8f06a52687c4a5dc12ee64fb7209c06
salman2610 <62196085+salman2610@users.noreply.github.com>
2025-07-14 15:00:30 +0530

    Update TextGeometry.js

--- assets/geometries/TextGeometry.js
+++ assets/geometries/TextGeometry.js
@@ -1,20 +1,3 @@
-/**
- * Text = 3D Text
- *
- * parameters = {
- *  font: <THREE.Font>, // font
- *
- *  size: <float>, // size of the text
- *  depth: <float>, // thickness to extrude text
- *  curveSegments: <int>, // number of points on the curves
- *
- *  bevelEnabled: <bool>, // turn on bevel
- *  bevelThickness: <float>, // how deep into text bevel goes
- *  bevelSize: <float>, // how far from text outline (including bevelOffset) is bevel
- *  bevelOffset: <float> // how far from text outline does bevel start
- * }
- */
-
 import {
 	ExtrudeGeometry
 } from 'three';
