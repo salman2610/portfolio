31fcfec80192993f837c096ae6c65f41148dcd4c
salman2610 <62196085+salman2610@users.noreply.github.com>
2025-07-14 15:45:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Ensure these imports are from the CDN (unpkg.com) for maximum reliability:
 import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
 import { FontLoader } from 'https://unpkg.com/three@0.165.0/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'https://unpkg.com/three@0.165.0/examples/jsm/geometries/TextGeometry.js'; // THIS LINE IS CORRECTED
